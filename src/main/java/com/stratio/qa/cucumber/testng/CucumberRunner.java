9e85f7732cc124ca821c5873c8b2ca85542847a9
alopez-stratio <33625777+alopez-stratio@users.noreply.github.com>
2019-09-04 09:08:27 +0200

    [QATM-2817] Add MarathonLB folder to classpath (#435)

--- src/main/java/com/stratio/qa/cucumber/testng/CucumberRunner.java
+++ src/main/java/com/stratio/qa/cucumber/testng/CucumberRunner.java
@@ -121,6 +121,7 @@ public class CucumberRunner {
         uniqueGlue.add("classpath:com/stratio/ignite/specs");
         uniqueGlue.add("classpath:com/stratio/qa/cucumber/converter");
         uniqueGlue.add("classpath:com/stratio/logs_solution/specs");
+        uniqueGlue.add("classpath:com/stratio/marathonlbsec/specs");
 
         runtimeOptions.getGlue().clear();
         runtimeOptions.getGlue().addAll(uniqueGlue);
